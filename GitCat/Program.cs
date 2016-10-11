a9de2f2401b3cb882b9e01783bf423f7b9a3310f
bf <bf@ENO-ESS-L020>
2016-10-11 14:29:50 +0200

    Test Commited.

--- GitCat/Program.cs
+++ GitCat/Program.cs
@@ -10,6 +10,8 @@ namespace GitCat
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
